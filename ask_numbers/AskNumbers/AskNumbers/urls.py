dab2d81461ffb904eca3f5d983f39aff4f7c2887
ivasik <51726661+Blackblackofficial@users.noreply.github.com>
2021-04-27 15:44:12 +0300

    Чистка проекта

--- ask_numbers/AskNumbers/AskNumbers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('ask.urls')),
-    path('', include('questions.urls')),
-]
